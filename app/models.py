999dfe4cf2a7ca142c35b6231528be85aca4d96e
Applied Algorithms Lab <appliedalgorithmslab@gmail.com>
2026-03-30 14:22:48 -0700

    add sqlalchemy models

---
+++ app/models.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column, Float, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from app.db import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False, index=True)
+
+    orders = relationship("Order", back_populates="user")
+
+
+class Order(Base):
+    __tablename__ = "orders"
+
+    id = Column(Integer, primary_key=True, index=True)
+    total = Column(Float, nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+
+    user = relationship("User", back_populates="orders")
